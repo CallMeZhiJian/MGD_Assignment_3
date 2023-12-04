f9e7cccd799ebad4e63cc487b7251ea617cf14fa
CallMeZhiJian <zjiandev@gmail.com>
2023-12-05 01:03:38 +0800

    1. Game Rules update (Check distance to middle of last block for score)
    2. Art assets all in

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -21,9 +21,13 @@ public class Block : MonoBehaviour
             {
                 Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y + 1.8f, Camera.main.transform.position.z);
                 gameManager.speed += 0.5f;
+                gameManager.scale -= 0.05f;
                 gameManager.SpawnBlock();
                 gameManager.AddScore(gameObject);
             }
+
+            gameManager.CineCam.m_LookAt = gameManager.blockPrev.transform;
+            gameManager.CineCam.m_Follow = gameManager.blockPrev.transform;
 
             Destroy(this);
         }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
+using Cinemachine;
 public class GameManager : MonoBehaviour
 {
+    public CinemachineVirtualCamera CineCam;
+
     //Reference for blocks
     public GameObject blockPrefab;
     private GameObject blockSpawned;
-    private Rigidbody blockRB;
+    [HideInInspector] public GameObject blockPrev;
+    private GameObject blockPrev2;
+    public Rigidbody blockRB;
 
     //Moving Position
     public GameObject movePositionParent;
@@ -20,25 +24,30 @@ public class GameManager : MonoBehaviour
     private float endTime;
     private float touchTime;
 
-    public bool changeMovePos;
+    private bool changeMovePos;
     private bool changeDirection;
 
     public static bool isDropped;
     public static bool isSpawned;
 
-    public float speed = 2;
-    public int score;
-    public int highScore;
+    [HideInInspector] public float speed = 2;
+    public float scale = 3;
+    private int score;
+    private int highScore;
+    private int blockCount;
 
     //UI stuffs
-    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI scoreTextInGame;
+    public TextMeshProUGUI scoreTextResult;
+    public TextMeshProUGUI highScoreText;
 
     private void Start()
     {
         isDropped = false;
         changeMovePos = true;
+        blockCount = 0;
         score = 0;
-        scoreText.text = score.ToString();
+        scoreTextInGame.text = score.ToString();
 
         SpawnBlock();
     }
@@ -76,22 +85,31 @@ public class GameManager : MonoBehaviour
 
     public void SpawnBlock()
     {
-        movePositionParent.transform.position = new Vector3(movePositionParent.transform.position.x, movePositionParent.transform.position.y + 2, movePositionParent.transform.position.z);
+        blockCount++;
+        if (blockPrev != null)
+        {
+            movePositionParent.transform.position = new Vector3(blockPrev.transform.position.x, movePositionParent.transform.position.y + scale, blockPrev.transform.position.z);
+        }
+        else
+        {
+            movePositionParent.transform.position = new Vector3(movePositionParent.transform.position.x, movePositionParent.transform.position.y + scale, movePositionParent.transform.position.z);
+        }
+        
 
         if (changeMovePos)
         {
-            Debug.Log("Spawn Left");
             Vector3 pos = new(movePosLeft[0].transform.position.x, movePositionParent.transform.position.y, movePosLeft[0].transform.position.z);
             blockSpawned = Instantiate(blockPrefab, pos, Quaternion.identity); 
         }
         else
         {
-            Debug.Log("Spawn Right");
             Vector3 pos = new(movePosRight[0].transform.position.x, movePositionParent.transform.position.y, movePosRight[0].transform.position.z);
             blockSpawned = Instantiate(blockPrefab, pos, Quaternion.identity);
         }
 
-        blockSpawned.GetComponent<MeshRenderer>().material.color = Random.ColorHSV();
+        blockSpawned.name = "Block" + blockCount;
+        //blockSpawned.GetComponent<MeshRenderer>().material.color = Random.ColorHSV();
+        blockSpawned.GetComponent<Transform>().localScale = new Vector3(scale, scale, scale);
 
         blockRB = blockSpawned.GetComponent<Rigidbody>();
         blockRB.useGravity = false;
@@ -143,8 +161,13 @@ public class GameManager : MonoBehaviour
 
     public void DropBlock()
     {
-        Debug.Log("Dropped");
         blockRB.useGravity = true;
+        if (blockPrev != null)
+        {
+            blockPrev2 = blockPrev;
+        }
+       
+        blockPrev = blockSpawned;
         blockSpawned = null;
 
         isSpawned = false;
@@ -156,36 +179,74 @@ public class GameManager : MonoBehaviour
         else
         {
             changeMovePos = true;
-        } 
+        }
     }
 
     public void AddScore(GameObject block)
     {
+        float distanceToMiddle;
+
+        Debug.Log(block.transform.position);
+        if (blockPrev2 != null)
+        {
+            Debug.Log(blockPrev2.transform.position);
+        }
+        
+
         if (!changeMovePos)
         {
-            Debug.Log("X");
-            if(block.transform.position.x <= 0.1 && block.transform.position.x >= -0.1)
+            if (blockPrev2 != null)
             {
+                distanceToMiddle = block.transform.position.x - blockPrev2.transform.position.x;
+            }
+            else
+            {
+                distanceToMiddle = block.transform.position.x - 0;
+            }
+            
+            Debug.Log("X : " + distanceToMiddle);
+
+            if (distanceToMiddle <= 0.15 && distanceToMiddle >= -0.15)
+            {
+                Debug.Log("2");
                 score += 2;
             }
             else
             {
+                Debug.Log("1");
                 score++;
             }
         }
         else
         {
-            Debug.Log("Z");
-            if(block.transform.position.z <= 0.1 && block.transform.position.z >= -0.1)
+            if (blockPrev2 != null)
             {
+                distanceToMiddle = block.transform.position.z - blockPrev2.transform.position.z;
+            }
+            else
+            {
+                distanceToMiddle = block.transform.position.z - 0;
+            }
+            
+            Debug.Log("Z : " + distanceToMiddle);
+
+            if (distanceToMiddle <= 0.15 && distanceToMiddle >= -0.15)
+            {
+                Debug.Log("2");
                 score += 2;
             }
             else
             {
+                Debug.Log("1");
                 score++;
             }
         }
 
-        scoreText.text = score.ToString();
+        if(score >= highScore)
+        {
+            highScore = score;
+        }
+
+        scoreTextInGame.text = score.ToString();
     }
 }
